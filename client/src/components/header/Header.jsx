c5fa13a63f9bda1dffe63a4bd2b1b40846f53358
Dishari Roy <roydishari7@gmail.com>
2024-03-29 14:16:20 +0530

    message

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+import head from "./img/head.jpg";
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">WELCOME TO PERSONALIZED</span>
+        <span className="headerTitleLg">HELLO-BLOGS</span>
+      </div>
+      <img className="headerImg" src={head} alt="" />
+    </div>
+  );
+}
